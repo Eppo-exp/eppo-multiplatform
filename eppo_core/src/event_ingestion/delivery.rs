0286935a197de181d79f55c9c0a7d8987ab83ef5
Felipe Lima <felipe.lima@gmail.com>
2025-01-17 13:14:58 -0800

    more code review comments

--- eppo_core/src/event_ingestion/delivery.rs
+++ eppo_core/src/event_ingestion/delivery.rs
@@ -15,53 +15,44 @@ pub(super) async fn delivery(
 ) -> Option<()> {
     loop {
         let event_delivery = event_delivery.clone();
-        match uplink.recv().await {
-            None => {
-                return None;
-            }
-            Some(BatchedMessage {
-                batch,
-                flush: _flush,
-            }) => {
-                let result = event_delivery.deliver(batch.clone()).await;
-                match result {
-                    Ok(response) => {
-                        let failed_event_uuids = response.failed_events;
-                        if !failed_event_uuids.is_empty() {
-                            warn!("Failed to deliver {} events", failed_event_uuids.len());
-                            let mut success = Vec::new();
-                            let mut failure = Vec::new();
-                            batch.into_iter().for_each(|queued_event| {
-                                if failed_event_uuids.contains(&queued_event.uuid) {
-                                    failure.push(queued_event);
-                                } else {
-                                    success.push(queued_event);
-                                }
-                            });
-                            delivery_status
-                                .send(DeliveryStatus { success, failure })
-                                .await
-                                .ok()?;
+        let BatchedMessage { batch, flush } = uplink.recv().await?;
+        let result = event_delivery.deliver(batch.clone()).await;
+        match result {
+            Ok(response) => {
+                let failed_event_uuids = response.failed_events;
+                if !failed_event_uuids.is_empty() {
+                    warn!("Failed to deliver {} events", failed_event_uuids.len());
+                    let mut success = Vec::new();
+                    let mut failure = Vec::new();
+                    for event in batch {
+                        if failed_event_uuids.contains(&event.uuid) {
+                            failure.push(event);
+                        } else {
+                            success.push(event);
                         }
                     }
-                    Err(err) => {
-                        match err {
-                            EventDeliveryError::RetriableError(_) => {
-                                // Retry later
-                                delivery_status
-                                    .send(DeliveryStatus {
-                                        failure: batch,
-                                        success: Vec::new(),
-                                    })
-                                    .await
-                                    .ok()?;
-                            }
-                            EventDeliveryError::NonRetriableError(_) => {
-                                warn!("Failed to deliver events: {}", err);
-                                // In this case there is no point in retrying delivery since the error is
-                                // non-retriable.
-                            }
-                        }
+                    delivery_status
+                        .send(DeliveryStatus { success, failure })
+                        .await
+                        .ok()?;
+                }
+            }
+            Err(err) => {
+                match err {
+                    EventDeliveryError::RetriableError(_) => {
+                        // Retry later
+                        delivery_status
+                            .send(DeliveryStatus {
+                                failure: batch,
+                                success: Vec::new(),
+                            })
+                            .await
+                            .ok()?;
+                    }
+                    EventDeliveryError::NonRetriableError(_) => {
+                        warn!("Failed to deliver events: {}", err);
+                        // In this case there is no point in retrying delivery since the error is
+                        // non-retriable.
                     }
                 }
             }
