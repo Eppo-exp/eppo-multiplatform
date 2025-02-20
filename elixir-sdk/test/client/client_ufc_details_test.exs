ca160ddfd666d5566657ce1819e73cd8047721ec
Sven Schmit <sven@geteppo.com>
2025-02-20 10:31:48 -0800

    clean up: no testing of details return

--- elixir-sdk/test/client/client_ufc_details_test.exs
+++ elixir-sdk/test/client/client_ufc_details_test.exs
@@ -85,25 +85,8 @@ defmodule EppoSdk.ClientUFCDetailsTest do
 
           assert value == expected_value,
                  "Failed for subject #{subject_key} in #{unquote(basename)}. Found value #{inspect(value)} but expected #{inspect(expected_value)}"
-
-          # TODO: Currently details returned by core library do not match test data.
-          # Assert that evaluation details match
-          # expected_details = subject["evaluationDetails"]
-          # if expected_details, do: assert_details_match(details, expected_details), else: :ok
         end)
       end
     end
   end
-
-  # defp assert_details_match(actual, expected) do
-  #   Enum.each(expected, fn {key, expected_value} ->
-  #     assert Map.has_key?(actual, key),
-  #            "Missing key #{inspect(key)} in actual details"
-
-  #     actual_value = Map.get(actual, key)
-
-  #     assert actual_value == expected_value,
-  #            "Value mismatch for key #{inspect(key)}\nExpected: #{inspect(expected_value)}\nGot: #{inspect(actual_value)}"
-  #   end)
-  # end
 end
