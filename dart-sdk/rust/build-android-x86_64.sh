d0ae2e76bde2eb2157b4e056e312994bcfbe1b10
Felipe Lima <felipe.lima@gmail.com>
2025-02-28 16:14:52 -0800

    build patches

--- dart-sdk/rust/build-android-x86_64.sh
+++ dart-sdk/rust/build-android-x86_64.sh
@@ -5,4 +5,4 @@
 export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/x86_64-linux-android35-clang"
 export RANLIB="$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-ranlib"
 export AR_x86_64_linux_android="$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-ar"
-cargo build --release --target x86_64-linux-android -p eppo_dart
+cargo build --release --target x86_64-linux-android -p eppo_sdk
