6cf737de28b6cd752645601503146f00273b7ba9
Felipe Lima <felipe.lima@gmail.com>
2025-03-03 15:22:16 -0800

    remove custom build scripts

--- dart-sdk/rust/build-android-x86_64.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Need to set NDK_HOME env var
-
-export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER="$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/x86_64-linux-android35-clang"
-export RANLIB="$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-ranlib"
-export AR_x86_64_linux_android="$NDK_HOME/toolchains/llvm/prebuilt/darwin-x86_64/bin/llvm-ar"
-cargo build --release --target x86_64-linux-android -p eppo_sdk
