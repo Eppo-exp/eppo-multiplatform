e0bcf87c82c2e11a0938ce441883c5a7dd95b13c
Leo Romanovsky <leo.romanovsky@datadoghq.com>
2025-10-13 07:10:35 -0400

    Remove unrelased `dart-sdk`

--- dart-sdk/hook/build.dart
+++
@@ -1,3 +0,0 @@
-import '../simple_build.dart';
-
-void main(List<String> args) async => simpleBuild(args);
--- dart-sdk/lib/src/logger.dart
+++
@@ -1,5 +0,0 @@
-class AssignmentLogger {
-  const AssignmentLogger();
-  void logAssignment(Map<String, dynamic> event) {}
-  void logBanditAction(Map<String, dynamic> event) {}
-}
